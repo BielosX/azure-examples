13503117cd810eae024c55bfa7252f917058b824
Mateusz Biel <matbiel@gmail.com>
2023-05-13 11:50:41 +0200

    Implement SHA256 calculation and blob tagging

--- blob-upload-trigger-function/CalculateSHA256/CalculateSHA256.fs
+++ blob-upload-trigger-function/CalculateSHA256/CalculateSHA256.fs
@@ -1,12 +1,24 @@
 namespace CalculateSHA256
 
+open System
 open System.IO
 open Microsoft.Azure.WebJobs
 open Microsoft.Extensions.Logging
+open Azure.Storage.Blobs
+open System.Security.Cryptography
 
 module CalculateSHA256 =
 
     [<FunctionName("CalculateSHA256")>]
     let run ([<BlobTrigger("demo-container/{name}", Connection="StorageConnectionString")>] myBlob: Stream, name: string, log: ILogger) =
-        let msg = sprintf "F# Blob trigger function Processed blob\nName: %s \n Size: %d Bytes" name myBlob.Length
+        let msg = sprintf "Blob\nName: %s \n Size: %d Bytes" name myBlob.Length
         log.LogInformation msg
+        let connString = Environment.GetEnvironmentVariable("StorageConnectionString")
+        let blobClient = BlobClient(connString, "demo-container", name)
+        let sha256 = SHA256.Create()
+        let shaValue = System.Convert.ToHexString(sha256.ComputeHash(myBlob))
+        let logSha = sprintf "Calculated SHA256: %s" shaValue
+        log.LogInformation logSha
+        let blobTags = blobClient.GetTags().Value.Tags
+        blobTags.Add("SHA256", shaValue)
+        blobClient.SetTags(blobTags) |> ignore
